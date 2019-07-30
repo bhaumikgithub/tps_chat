79d71ad227565bb76ef8b2e592bfc8d894aedc92
Tanaytatva <tanay.sharma@tatvasoft.com>
2019-07-30 14:14:47 +0530

    fixed untracked files

--- app/assets/javascripts/channels/conversation.js
+++
@@ -1,8 +0,0 @@
-App.conversation = App.cable.subscriptions.create("ConversationChannel", {
-  connected: function() {},
-  disconnected: function() {},
-  received: function(data) {},
-  speak: function() {
-    return this.perform('speak');
-  }
-});
--- db/seeds.rb
+++
@@ -1,8 +0,0 @@
-password = 'pass123'
-1.upto(5) do |i|
-  User.create(
-    email: "user#{i}@gmail.com",
-    password: password,
-    password_confirmation: password
-  )
-end
