52748436c3fd4bd31c320e02bf511e529753a57f
tps <nency.tps@gmail.com>
2019-07-31 12:56:00 +0530

    chat app

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,8 @@ set :repo_url, "git@github.com:bhaumikgithub/tps_chat.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "project/tps_chat"
-set :deploy_to, 'project/demo'
+set :deploy_to, "/home/ubuntu/project/tps_chat"
+# set :deploy_to, 'project/demo'
 set :rvm_ruby_version, '2.6.3'
 
 set :passenger_restart_with_touch, true
@@ -20,6 +20,7 @@ set :branch, "master"
 # set :use_sudo, true
 set :user, 'ubuntu'
 
+set :pty, true
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 # role :app, %w{ubuntu@13.233.109.182}
 # role :web, %w{ubuntu@13.233.109.182}
